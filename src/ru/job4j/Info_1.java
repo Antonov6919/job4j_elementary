ed63bd8a3b9bf9e3933b4219851b3e5142c7938b
Андрей <akvilona@list.ru>
2021-04-22 18:12:15 +0300

    Загружаем новый класс ru.job4j.Multiple

--- src/ru/job4j/Info_1.java
+++ src/ru/job4j/Info_1.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-public class Info {
-    public static void main(String[] args) {
+public class Info_1 {
+    public static void main (String[] args) {
         System.out.println("22.04.2021");
     }
 }
